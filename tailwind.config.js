02f044f8ad3ee7a0850633f834bb643b0ada5942
Colinloic <39315191+Colinloic@users.noreply.github.com>
2022-11-20 22:15:58 +0100

    template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  experimental: {
+    optimizeUniversalDefaults: true
+  },
   content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
+    './assets/**/*.js',
+    './templates/**/*.html.twig',
+    './node_modules/tw-elements/dist/js/**/*.js'
   ],
   theme: {
+    colors: {
+      'yellow': '#FFCC03',
+      'blue': '#386ABB',
+      'dark': '#000000',
+      'white': '#FFFFFF',
+      'type_eau': '#399CFF'
+
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ],
 }
