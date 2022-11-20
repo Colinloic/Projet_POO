8488a2adb1635ff2ceeefbc9fc42ffa1eff08359
Colinloic <39315191+Colinloic@users.noreply.github.com>
2022-11-20 22:19:12 +0100

    Revert "template"
    This reverts commit 02f044f8ad3ee7a0850633f834bb643b0ada5942.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  experimental: {
-    optimizeUniversalDefaults: true
-  },
   content: [
-    './assets/**/*.js',
-    './templates/**/*.html.twig',
-    './node_modules/tw-elements/dist/js/**/*.js'
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
   ],
   theme: {
-    colors: {
-      'yellow': '#FFCC03',
-      'blue': '#386ABB',
-      'dark': '#000000',
-      'white': '#FFFFFF',
-      'type_eau': '#399CFF'
-
-    },
     extend: {},
   },
-  plugins: [
-    require('tw-elements/dist/plugin')
-  ],
+  plugins: [],
 }
